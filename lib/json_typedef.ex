d16440cd3afca69bd0bfba7af803571f8cca143d
Pascal736 <pascal_pfeiffer@outlook.de>
2026-01-28 07:49:51 +0100

    initial commit

---
+++ lib/json_typedef.ex
@@ -0,0 +1,18 @@
+defmodule JsonTypedef do
+  @moduledoc """
+  Documentation for `JsonTypedef`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JsonTypedef.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/json_typedef_test.exs
@@ -0,0 +1,8 @@
+defmodule JsonTypedefTest do
+  use ExUnit.Case
+  doctest JsonTypedef
+
+  test "greets the world" do
+    assert JsonTypedef.hello() == :world
+  end
+end
