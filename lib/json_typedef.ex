ec621f7ca11f72d83fe1a05e442c6f6d6ce0092d
Pascal736 <pascal_pfeiffer@outlook.de>
2026-01-29 08:58:52 +0100

    WIP valid_schema? function

--- lib/json_typedef.ex
+++ lib/json_typedef.ex
@@ -1,27 +1,32 @@
 defmodule JsonTypedef do
-  @moduledoc """
-  Documentation for `JsonTypedef`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> JsonTypedef.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
-  def validate(schema, data) do
+  def validate(_schema, _data) do
     # Add 'allow_additional_properties?' mode. Having field: 'additionalProperties': true in the schema turns this mode on.
   end
 
-  def valid_schema?(schema) do
+  def valid_schema?(schema) when is_map(schema) do
+    case internal_valid_schema?(schema, :root) do
+      true -> {:ok, true}
+      false -> {:ok, false}
+    end
   end
+
+  defp internal_valid_schema?(%{"definitions" => defs} = schema, :root)
+       when is_map(defs) do
+    Enum.all?(defs, fn {_name, def_schema} ->
+      internal_valid_schema?(def_schema, :non_root)
+    end) and
+      internal_valid_schema?(Map.delete(schema, "definitions"), :non_root)
+  end
+
+  defp internal_valid_schema?(%{"definitions" => _}, :non_root), do: false
+
+  defp internal_valid_schema?(schema, level) when is_map(schema) do
+    Enum.all?(schema, fn {_k, v} ->
+      internal_valid_schema?(v, level)
+    end)
+  end
+
+  defp internal_valid_schema?(_other, _level), do: true
 end
 
 # root schema -> Top level schema
--- test/json_typedef_test.exs
+++ test/json_typedef_test.exs
@@ -1,8 +1,4 @@
 defmodule JsonTypedefTest do
   use ExUnit.Case
   doctest JsonTypedef
-
-  test "greets the world" do
-    assert JsonTypedef.hello() == :world
-  end
 end
