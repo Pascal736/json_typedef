696f249b21cdb944bdfe00c9c87d455abeae21a7
Pascal736 <pascal_pfeiffer@outlook.de>
2026-02-03 18:08:25 +0100

    Add type hints

--- lib/error_path.ex
+++ lib/error_path.ex
@@ -1,4 +1,9 @@
 defmodule JsonTypedef.ErrorPath do
   @enforce_keys [:instance_path, :schema_path]
   defstruct instance_path: [], schema_path: []
+
+  @type t :: %__MODULE__{
+          instance_path: [String.t() | integer()],
+          schema_path: [String.t()]
+        }
 end
