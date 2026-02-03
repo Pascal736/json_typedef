479b926927fb99ccda77d2ce34c8dc9885093adf
Pascal736 <pascal_pfeiffer@outlook.de>
2026-02-03 18:12:51 +0100

    Minimal mix project configuration

--- mix.exs
+++ mix.exs
@@ -4,25 +4,26 @@ defmodule JsonTypedef.MixProject do
   def project do
     [
       app: :json_typedef,
-      version: "0.1.0",
+      description: "Implementation of rfc8927 to validate JSON typedef structures.",
+      version: "0.0.1",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: [],
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp package do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      licenses: ["MIT"],
+      maintainers: ["Pascal Pfeiffer"],
+      links: %{
+        "GitHub" => "https://github.com/pascal736/json_typedef"
+      }
     ]
   end
 end
